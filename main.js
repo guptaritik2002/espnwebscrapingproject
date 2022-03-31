6e076939cfb4de87d1588b8e642adc7d5c1defb5
guptaritik2002 <gupta.ritik2002@gmail.com>
2022-03-31 22:40:54 +0530

    linked main.js file with another files

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const request = require("request");
 const cheerio = require("cheerio");
+const fs=require("fs");
+const path=require("path");
+
+const iplpath=path.join(__dirname,"ipl");
+dircreater(iplpath);
+
 const viewallpageObj = require("./viewallpage");
 const url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595";
 request(url, cb);
@@ -18,4 +24,12 @@ function extracthtml(html) {
   let fulllink = "https://www.espncricinfo.com" + link;
   // console.log(fulllink);
   viewallpageObj.allviewpage(fulllink);
+}
+
+function dircreater(filepath)
+{
+  if(fs.existsSync(filepath)==false)
+  {
+    fs.mkdirSync(filepath);
+  }
 }
